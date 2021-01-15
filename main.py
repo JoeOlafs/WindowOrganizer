14e68fcfe197852a2e02537e0bf1cc08090230b3
JoeOlafs <jaolafsson@hotmail.com>
2021-01-15 22:48:14 +0200

    Work in Progress

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 "#WindowOrganizer" 
 import pygetwindow as gw
 from itertools import groupby
+def isMin():
+    #isMinimized
+    return bool
 
-openWindow = ([1, 2, 3])
+openWindow = []
 allWindows = [gw.getAllTitles()]
 #print(gw.getActiveWindow().title)
 
 #print(allWindows)
 Window = allWindows[0]
-print(openWindow)
+print(type(allWindows))
+
+for x in allWindows:
+    openWindow = x
+    print(openWindow)
--- test.py
+++ test.py
@@ -9,3 +9,16 @@ a = [1,2,3,4]
 group_obj = groupby(a,key=lambda x: x<3) #can also be the function defined above
 for key, value in group_obj:
      print(key,list(value))
+group_obj2 = groupby(persons, key=lambda x: x['age'])
+for key, value in group_obj2:
+     print(key,list(value))
+
+mylist = [1,2,3]
+for x in mylist:
+     print(x)
+
+notepadWindow = gw.getWindowsWithTitle('Untitled')[0]
+if notepadWindow.isMinimized:
+    print("true")
+else:
+    print("false")
