07c422534d5805dc9d538891143c28d2cf4b02c3
JoeOlafs <jaolafsson@hotmail.com>
2021-01-19 19:40:19 +0200

    Reorganisation and adding to .gitignor

--- main3.py
+++ main3.py
@@ -1,7 +1,12 @@
+#Window Organizer
+#Created by Johannes Olafsson - github.com/joeolafs
+#Resizes and organizes open windows on your desktop
+
 from pywinauto import Desktop
 
 windows = Desktop(backend="uia").windows()
 windowList = ([w.window_text() for w in windows])
-if 'Taskbar', '', 'Overwolf Quick Launcher' in windowList:
-    windowList.remove('Taskbar', '', 'Overwolf Quick Launcher')
-    print(windowList)
+print(windowList)
+#if 'Taskbar', '', 'Overwolf Quick Launcher' in windowList:
+ #   windowList.remove('Taskbar', '', 'Overwolf Quick Launcher')
+  #  print(windowList)
