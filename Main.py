ee030b29ea64ff071977c14e93bf1955295dcc3f
JoeOlafs <jaolafsson@hotmail.com>
2021-02-06 16:10:15 +0200

    update file names - add next steps to Main

--- Main.py
+++ Main.py
@@ -1,21 +1,26 @@
-#Window Organizer
-#Created by Johannes Olafsson - github.com/joeolafs
-#Resizes and organizes open windows on your desktop
+from pywinauto import Desktop
+import pygetwindow as gw
+import ExcludeFromList
+import Visibility
+import ResizeSingle
+import ResizeDual
+import ListMonitors
 
-import time
-import RunScript
+def __job__():
+     windows = Desktop(backend="uia").windows()
+     windowList = ([w.window_text() for w in windows])
+     ExcludeFromList.__remove__(windowList)
+     Visibility.__notMinimized__(windowList)
+     print("List length: " + str(len(windowList)))
 
-start = time.time()
-errCount = 0
-
-if __name__ == '__main__':
-    while True:
-        try:
-            RunScript.__job__()
-        except IndexError as err:
-            errCount += 1
-        finally:
-            timer = time.time()-start
-            print('err count: ' + str(errCount))
-            print(f'has been running for: '+ str(timer))
-            time.sleep(1)
+     # add if loop - int = len(windowList) stays the same -> do nothing
+     # else - run rest of script and update int = len(windowList)
+     for program in windowList:
+          Window = gw.getWindowsWithTitle(program)[0]
+          print(Window)
+     monitors = ListMonitors.Monitors()
+     print(len(monitors))
+     #if len(monitors) == 1:
+     #     ResizeSingle.__resize__(windowList)
+     #if len(monitors) == 2:
+     #     ResizeDual.__resize__(windowList)
---
+++ Run.py
@@ -0,0 +1,21 @@
+#Window Organizer
+#Created by Johannes Olafsson - github.com/joeolafs
+#Resizes and organizes open windows on your desktop
+
+import time
+import Main
+
+start = time.time()
+errCount = 0
+
+if __name__ == '__main__':
+    while True:
+        try:
+            Main.__job__()
+        except IndexError as err:
+            errCount += 1
+        finally:
+            timer = time.time()-start
+            print('err count: ' + str(errCount))
+            print(f'has been running for: '+ str(timer))
+            time.sleep(1)
