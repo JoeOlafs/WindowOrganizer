5b858f587d0f3247c2d8b7e58985d02ed464180e
JoeOlafs <jaolafsson@hotmail.com>
2021-02-07 16:50:03 +0200

    Adding comments

--- Main.py
+++ Main.py
@@ -8,21 +8,28 @@ import ListMonitors
 import Run
 
 def __job__():
+     # Gets all open windows, runs through list of excluded programs and sorts by visibility
      windows = Desktop(backend="uia").windows()
      windowList = ([w.window_text() for w in windows])
      ExcludeFromList.__remove__(windowList)
      Visibility.__notMinimized__(windowList)
+     
+     # Checks for ammount of monitors
      monitors = ListMonitors.Monitors()
 
+     # Checks if a new window has been opened or a window has been closed
      if len(windowList) == Run.numWindow:
           print('Number of window stays the same')
      else:
-          Run.numWindow = len(windowList)
+          Run.numWindow = len(windowList) # Updates number of open windows
           print("List length: " + str(len(windowList)))
+          # Additional checks to see window info if running in VS code
           for program in windowList:
                Window = gw.getWindowsWithTitle(program)[0]
                print(Window)
           print(len(monitors))
+          
+          # Arrange windows depending on number fo connected monitors
           if len(monitors) == 1:
                ResizeSingle.__resize__(windowList)
           if len(monitors) == 2:
--- Visibility.py
+++ Visibility.py
@@ -1,8 +1,11 @@
+# Function to remove programs that have been minimized
 import pygetwindow as gw
 
 def __notMinimized__(list):
     isMinimized = []
     isNotMinimized = []
+
+    # Arrange programs depening if they have been minimized or not
     for program in list:
         Window = gw.getWindowsWithTitle(program)[0]
         if Window.isMinimized:
@@ -10,6 +13,7 @@ def __notMinimized__(list):
         elif Window.isMinimized == False:
             isNotMinimized.append(program)
 
+    # Remove programs that have been minimized from list of open programs
     for prog in isMinimized:
         if prog in list:
             list.remove(prog)
