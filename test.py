d72a1ab0ee7034ab7b3e3e798d86d64157045989
JoeOlafs <jaolafsson@hotmail.com>
2021-02-08 19:15:14 +0200

    change method to get display info

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import Main
-list = [1,2,3,4,5]
-list = tuple(list)
+import subprocess
+import re
 
-print(type(list))
+proc = subprocess.Popen(['powershell', 'Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams'], stdout=subprocess.PIPE)
+res = proc.communicate()
+monitors = re.findall('(?s)\r\nInstanceName\s+:\s(.*?)\r\n',res[0].decode("utf-8"))
+for m in monitors:
+     print(m)
