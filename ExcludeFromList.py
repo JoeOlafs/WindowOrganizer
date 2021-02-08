a63ca13542721d17565614b10ba1ec6b8d1a2b9e
JoeOlafs <jaolafsson@hotmail.com>
2021-02-08 12:06:05 +0200

    attempting to fix bug

--- ExcludeFromList.py
+++ ExcludeFromList.py
@@ -1,18 +1,14 @@
-# Function to remove selected programs from list of open programs
 def __remove__(list):
     inList = []
 
-    # List of programs and overlays that will not be arranged by the program
     exclude = ['Taskbar', 'Overwolf Quick Launcher',
      'NVIDIA GeForce Overlay', 'Screen share viewing options',
      'Program Manager', '']
 
-    # Add programs that are open to a list, 
     for program in exclude:
         if program in list:
             inList.append(program)
     
-    # Removes open programs that are in list to exclude from all open windows  
     for prog in inList:
         while prog in list:
             if prog in list:
--- Run.py
+++ Run.py
@@ -9,14 +9,13 @@ start = time.time()
 errCount = 0
 numWindow = 0
 
-# Program runs every second
 if __name__ == '__main__':
     while True:
         try:
             Main.__job__()
-        except IndexError as err: # Ignores common error in program, keeps track of how often it occurs
+        except IndexError as err:
             errCount += 1
-        finally: # Additional info while running in VS code
+        finally:
             timer = time.time()-start
             print('err count: ' + str(errCount))
             print(f'has been running for: '+ str(timer))
