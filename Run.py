c4dd419a6a1cf37cec71578e2a931b3f2b8070c2
JoeOlafs <jaolafsson@hotmail.com>
2021-02-13 15:13:35 +0200

    back to Startpoint of SysTrayApp - currently works

--- Run.py
+++ Run.py
@@ -6,33 +6,29 @@ import time
 import Main
 import traceback
 import ListMonitors
-import SysTrayRun
 
 start = time.time()
 numWindow = 0
 monitors = ListMonitors.Monitors()
+run_main = False
 
 # The program runs constantly
 def MainApp():
     #if __name__ == '__main__':
     errCount = 0
+
     while True:
-        try:
-            Main.__job__()
-        except IndexError as err: # Ignores an common error in program, keeps track of how often it occurs
-            traceback.print_exc()
-            errCount += 1
-        except:
-            traceback.print_exc()
-            errCount += 1
-        finally: # Additional info while running in an IDE
-            timer = time.time()-start
-            print('err count: ' + str(errCount))
-            print(f'has been running for: '+ str(timer))
-            time.sleep(1)
-
-
-SysTrayRun.SysTray()
-while True:
-    if SysTrayRun.run_main:
-        print('run')
+        if run_main:
+            try:
+                Main.__job__()
+            except IndexError as err: # Ignores an common error in program, keeps track of how often it occurs
+                traceback.print_exc()
+                errCount += 1
+            except:
+                traceback.print_exc()
+                errCount += 1
+            finally: # Additional info while running in an IDE
+                timer = time.time()-start
+                print('err count: ' + str(errCount))
+                print(f'has been running for: '+ str(timer))
+                time.sleep(1)
