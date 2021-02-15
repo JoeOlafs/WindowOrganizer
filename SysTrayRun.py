98ef3cc429a44c1bab6b937a580d932540d07e01
JoeOlafs <jaolafsson@hotmail.com>
2021-02-15 13:04:16 +0200

    Small updates and tests

--- SysTrayRun.py
+++ SysTrayRun.py
@@ -23,69 +23,78 @@ run_main = False
 err_count = 0
 start = time.time()
 
-while True:
-     if window_visible:
-          event,values = window.Read()
-          if event is None:
-               tray.UnHide()
-               tray_visible = True
-               window_visible = False
-               window_close = True
-          elif event == 'Close':
-               print('Minimize to Tray')
-               window.Hide()
-               tray.UnHide()
-               tray_visible = True
-               window_visible = False
-               window_close = True
-     if tray_visible:
-          menu_item = tray.Read()
-          if menu_item == 'Run' or menu_item == sg.EVENT_SYSTEM_TRAY_ICON_ACTIVATED:
-               print('Running Main')
-               tray.UnHide()
-               tray_visible = True
-               window_visible = False
-               window_close = True
-               run_main = True
-               while run_main == True:
-                    menu_item_running = tray.Read(timeout = 2000)
-                    if menu_item_running == 'Stop' or menu_item_running == sg.EVENT_SYSTEM_TRAY_ICON_ACTIVATED:
-                         print('Stopping Main')
-                         tray.UnHide()
-                         tray_visible = True
-                         window_visible = False
-                         window_close = True
-                         run_main = False
+def SysTray():
+
+     global tray_visible
+     global window_visible
+     global window_close
+     global run_main
+     global err_count
+     global start
+     
+     while True:
+          if window_visible:
+               event,values = window.Read()
+               if event is None:
+                    tray.UnHide()
+                    tray_visible = True
+                    window_visible = False
+                    window_close = True
+               elif event == 'Close':
+                    print('Minimize to Tray')
+                    window.Hide()
+                    tray.UnHide()
+                    tray_visible = True
+                    window_visible = False
+                    window_close = True
+          if tray_visible:
+               menu_item = tray.Read()
+               if menu_item == 'Run' or menu_item == sg.EVENT_SYSTEM_TRAY_ICON_ACTIVATED:
+                    print('Running Main')
+                    tray.UnHide()
+                    tray_visible = True
+                    window_visible = False
+                    window_close = True
+                    run_main = True
+                    while run_main == True:
+                         menu_item_running = tray.Read(timeout = 2000)
+                         if menu_item_running == 'Stop' or menu_item_running == sg.EVENT_SYSTEM_TRAY_ICON_ACTIVATED:
+                              print('Stopping Main')
+                              tray.UnHide()
+                              tray_visible = True
+                              window_visible = False
+                              window_close = True
+                              run_main = False
+                         else:
+                              try:
+                                   Main.__job__()    
+                              except IndexError as err: # Ignores an common error in program, keeps track of how often it occurs
+                                   traceback.print_exc()
+                                   err_count += 1
+                              except:
+                                   traceback.print_exc()
+                                   err_count += 1
+                              finally: # Additional info while running in an IDE
+                                   timer = time.time()-start
+                                   print('err count: ' + str(err_count))
+                                   print(f'has been running for: '+ str(timer))
+                                   #time.sleep(1)
+               elif menu_item == 'Stop' or menu_item == sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED:
+                    print('Stopping Main')
+                    tray.UnHide()
+                    tray_visible = True
+                    window_visible = False
+                    window_close = True
+                    run_main = False
+               elif menu_item == 'Info':
+                    tray.Hide()
+                    tray_visible = False
+                    window_visible = True
+                    window_close = False
+                    if window_close:
+                         layout = [[sg.Text('Window Organizer')],[sg.Button('Close')]]
+                         window = sg.Window('Window Organizer').Layout(layout)
                     else:
-                         try:
-                              Main.__job__()    
-                         except IndexError as err: # Ignores an common error in program, keeps track of how often it occurs
-                              traceback.print_exc()
-                              err_count += 1
-                         except:
-                              traceback.print_exc()
-                              err_count += 1
-                         finally: # Additional info while running in an IDE
-                              timer = time.time()-start
-                              print('err count: ' + str(err_count))
-                              print(f'has been running for: '+ str(timer))
-                              #time.sleep(1)
-          elif menu_item == 'Stop' or menu_item == sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED:
-               print('Stopping Main')
-               tray.UnHide()
-               tray_visible = True
-               window_visible = False
-               window_close = True
-               run_main = False
-          elif menu_item == 'Info':
-               tray.Hide()
-               tray_visible = False
-               window_visible = True
-               window_close = False
-               if window_close:
-                    layout = [[sg.Text('Window Organizer')],[sg.Button('Close')]]
-                    window = sg.Window('Window Organizer').Layout(layout)
-               else:
-                    window.UnHide()
-          elif menu_item == 'Close':
-               break
+                         window.UnHide()
+               elif menu_item == 'Close':
+                    break
